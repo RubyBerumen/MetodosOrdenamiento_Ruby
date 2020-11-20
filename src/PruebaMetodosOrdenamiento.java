38d20b794a9ae1094fc67129b68a6dc5efbda4d1
Ruby Berumen <clararubipb15@gmail.com>
2020-11-19 23:29:17 -0600

    Se agregarón los métodos de ordenamiento de burbuja2 y burbuja3

--- src/PruebaMetodosOrdenamiento.java
+++ src/PruebaMetodosOrdenamiento.java
@@ -7,7 +7,7 @@ class MetodosOrdenamiento{
 		static long tInicio, tFin;
 		
 		public static void ordenacionBurbuja1(int [] numeros) {
-			
+			tInicio=System.nanoTime();
 			for (int i=1; i<=numeros.length-1; i++) {
 				for (int j=0; j<=numeros.length-i-1; j++) {
 					if (numeros[j]>numeros[j+1]) {
@@ -17,13 +17,46 @@ class MetodosOrdenamiento{
 					}
 				}
 			}
-			tFin = System.currentTimeMillis();
-			System.out.println("Tiempo de ejecucion en ordenamiento por burbuja:"+(tFin-tInicio));
+			tFin = System.nanoTime();
+			System.out.println("Tiempo de ejecucion en ordenamiento por burbuja1:"+(tFin-tInicio));
 		}// Burbuja1
 		
-		public void ordenacionBurbuja2(int [] numeros) {}
+		public static void ordenacionBurbuja2(int [] numeros) {
+			tInicio = System.nanoTime();
+            for(int i=1; i<numeros.length; i++) {
+				
+				for(int j=0; j<numeros.length-i; j++) {
+					
+					if(numeros[j]>numeros[j+1]) {
+						int aux = numeros[j];
+						numeros[j] = numeros[j+1];
+						numeros[j+1] = aux;
+					}
+				}
+			}
+            tFin = System.nanoTime();
+			System.out.println("Tiempo de ejecucion en ordenamiento por burbuja2: " + (tFin-tInicio));
+		}// Burbuja2
 		
-		public void ordenacionBurbuja3(int [] numeros) {}
+		public static void ordenacionBurbuja3(int [] numeros) {
+			 tInicio = System.nanoTime();
+	           int i=1;
+	     	   do {
+	     		   
+	     		   for(int j=0; j<numeros.length-i; j++) {
+	 					
+	 					if(numeros[j]>numeros[j+1]) {
+	 						int aux = numeros[j];
+	 						numeros[j] = numeros[j+1];
+	 						numeros[j+1] = aux;
+	 					}
+	 				}
+	     		   
+	     		   i=1+i;
+	     	   }while(i<numeros.length);
+	     	    tFin = System.nanoTime();
+				System.out.println("Tiempo de ejecucion en ordenamiento por burbuja3: " + (tFin-tInicio));
+		}// Burbuja3
 		
 		public static void mostrarVector(int [] numeros) {
 			System.out.println(Arrays.toString(numeros));
@@ -42,6 +75,14 @@ public class PruebaMetodosOrdenamiento {
 		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
 		
 		MetodosOrdenamiento.Burbuja.ordenacionBurbuja1(numeros);
+		
+		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
+		
+		MetodosOrdenamiento.Burbuja.ordenacionBurbuja2(numeros);
+		
+		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
+		
+		MetodosOrdenamiento.Burbuja.ordenacionBurbuja3(numeros);
 		
 		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
 		
