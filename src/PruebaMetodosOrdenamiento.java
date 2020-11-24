714f697178d958074f1c944b14602938d5ef14ec
Ruby Berumen <clararubipb15@gmail.com>
2020-11-24 14:46:32 -0600

    Se agregó el metodo de ordenamiento por selección y el menú

--- src/PruebaMetodosOrdenamiento.java
+++ src/PruebaMetodosOrdenamiento.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 class MetodosOrdenamiento{
 	
@@ -65,60 +67,155 @@ class MetodosOrdenamiento{
 	}//class Burbuja
 	
 	
-}//class MetodosOrdenamiento
-
-
-class Insercion{
 	
-	public void ordenar(int[]numeros) {
-		int aux;
+	static class Insercion{
 		
-		for (int i = 0; i < numeros.length; i++) {
-			aux = numeros[i];
+		public static void ordenar(int[]numeros) {
+			int aux;
 			
-			for (int j = i-1; j > 0 && numeros[j]>aux; j--) {
-				numeros[j+1]=numeros[j];
-				numeros[j]=aux;
+			for (int i = 0; i < numeros.length; i++) {
+				aux = numeros[i];
+				
+				for (int j = i-1; j > 0 && numeros[j]>aux; j--) {
+					numeros[j+1]=numeros[j];
+					numeros[j]=aux;
+				}
+				
 			}
 			
 		}
-		
-	}
+	}//class insercion
 	
-	public void mostrarVector(int [] numeros) {
-		System.out.println(Arrays.toString(numeros));
+	
+	
+	static class Seleccion{
+		
+		public static void ordenamiento(int []numeros) {
+			for (int i = 0; i < numeros.length-1; i++) {
+				for (int j = i; j < numeros.length; j++) {
+					if(numeros[i]> numeros[j]) {
+						int minimo = numeros[i];
+						numeros[i]=numeros[j];
+						numeros [j] = minimo;
+					}
+					
+				}
+			}
+		}
+		
+	}//class Seleccion
+	
+	
+	public static void mostrarVector(int [] numeros) {
+		System.out.println("Vector ordenado: "+Arrays.toString(numeros) + "\n");
 	}
+
+}//class MetodosOrdenamiento
+
+
+
+	
 	
 
 	
-}//class insercion
+
+
+
+
+
 
 public class PruebaMetodosOrdenamiento {
 
 	public static void main(String[] args) {
 		
-		int numeros [] = {6,2,9,7,1,0,4};
+		Scanner ent = new Scanner(System.in);		
+		boolean salir = false;
 		
-		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
+		do {
+			int numeros [] = {6,2,9,7,1,0,4};
+			System.out.println("Vector desordenado: " + Arrays.toString(numeros) + "\n");
+			System.out.println("Ordenar el arreglo por método de:");
+			System.out.println("1. Burbuja");
+            System.out.println("2. Inserción");
+            System.out.println("3. Selección ");
+            System.out.println("4. Salir");
+           
+            try {
+            	System.out.println("Escribe una de las opciones");
+                int op = ent.nextInt();
+ 
+                switch (op) {
+                    case 1:
+                        
+                		boolean salir1 = false;
+                		
+                		do {
+                			
+                			System.out.println("1. Burbuja1");
+                            System.out.println("2. Burbuja2");
+                            System.out.println("3. Burbuja3");
+                            System.out.println("4. Salir");
+                           
+                            try {
+                            	System.out.println("Escribe una de las opciones");
+                                int opc = ent.nextInt();
+                 
+                                switch (opc) {
+                                    case 1:
+                                    	MetodosOrdenamiento.Burbuja.ordenacionBurbuja1(numeros);
+                                    	MetodosOrdenamiento.mostrarVector(numeros);
+                                        break;
+                                    case 2:
+                                    	MetodosOrdenamiento.Burbuja.ordenacionBurbuja2(numeros);
+                                    	MetodosOrdenamiento.mostrarVector(numeros);
+                                        break;
+                                    case 3:
+                                    	MetodosOrdenamiento.Burbuja.ordenacionBurbuja3(numeros);
+                                    	MetodosOrdenamiento.mostrarVector(numeros);
+                                        break;
+                                    case 4:
+                                        salir1 = true;
+                                        break;
+                                    default:
+                                        System.out.println("Debes ingresar números entre 1 y 4");
+                                }
+                				
+                			} catch (InputMismatchException e) {
+                				System.out.println("Debes ingresar un número");
+                                ent.next();
+                				
+                			}
+                            
+                			
+                		}while (!salir1);//while
+
+                        break;
+                    case 2:
+                    	MetodosOrdenamiento.Insercion.ordenar(numeros);
+                    	MetodosOrdenamiento.mostrarVector(numeros);
+                        break;
+                    case 3:
+                    	MetodosOrdenamiento.Seleccion.ordenamiento(numeros);
+                    	MetodosOrdenamiento.mostrarVector(numeros);
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Debes ingresar números entre 1 y 4");
+                }
+				
+			} catch (InputMismatchException e) {
+				System.out.println("Debes ingresar un número");
+                ent.next();
+				
+			}
+            
+			
+		}while (!salir);//while
 		
-		MetodosOrdenamiento.Burbuja.ordenacionBurbuja1(numeros);
-		
-		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
-		
-		MetodosOrdenamiento.Burbuja.ordenacionBurbuja2(numeros);
-		
-		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
-		
-		MetodosOrdenamiento.Burbuja.ordenacionBurbuja3(numeros);
-		
-		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
 		
 		
-		Insercion i = new Insercion();
-		System.out.println("Vector ordenado por metodo de inserción");
-		i.ordenar(numeros);
-		i.mostrarVector(numeros);
-	
 		
 
 
